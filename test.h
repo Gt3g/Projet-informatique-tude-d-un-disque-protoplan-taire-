a34e0d2d099b8fa72beb146da874c5e52610501c
Gt3g <nilsraynaud5@gmail.com>
2026-02-18 10:42:39 +0100

    Add files via upload

---
+++ test.h
@@ -0,0 +1,8 @@
+particule.h
+class Particule {
+public: // visibilité des membres (cf. cours encapsulation)
+// Déclaration des attributs = données membres
+double masse; // kg
+double charge; // C
+vec3 impulsion; // kg·m/s
+};
